6a33237bed5b92c42d4eaacbd344e2aa58ce44e6
tcye <tiancai.ye@outlook.com>
2017-03-26 14:29:08 +0800

    reformat code style for examples

--- examples/echo_client.cc
+++ examples/echo_client.cc
@@ -4,50 +4,60 @@
 
 #include "type_alias.h"
 
-class EchoClient : private hpt::noncopyable, public std::enable_shared_from_this<EchoClient> {
- public:
-  using SelfType = EchoClient;
+class EchoClient
+        : private hpt::noncopyable,
+          public std::enable_shared_from_this<EchoClient>
+{
+public:
+    using SelfType = EchoClient;
 
-  EchoClient() {
-    _service = std::make_shared<hpt::IoService>();
-    _socket = std::make_shared<hpt::Socket>(*_service);
-  }
-
-  hpt::IoService &GetIoService() { return *_service; }
-
-  void TryConnect(hpt::Endpoint endpoint) {
-    _socket->async_connect(endpoint, MEM_FN(OnConnect, _1));
-  }
-
-  void OnConnect(const asio::error_code &ec) {
-    if (ec) {
-      std::cerr << "Connect Error: " << ec << std::endl;
-      return;
+    EchoClient()
+    {
+        _service = std::make_shared<hpt::IoService>();
+        _socket = std::make_shared<hpt::Socket>(*_service);
     }
-  }
 
-  void Run() {
-    _service->run();
-  }
+    hpt::IoService& GetIoService() { return *_service; }
 
- private:
-  hpt::IoServicePtr _service;
-  hpt::SocketPtr _socket;
+    void TryConnect(hpt::Endpoint endpoint)
+    {
+        _socket->async_connect(endpoint, MEM_FN(OnConnect, _1));
+    }
+
+    void OnConnect(const asio::error_code& ec)
+    {
+        if (ec)
+        {
+            std::cerr << "Connect Error: " << ec << std::endl;
+            return;
+        }
+    }
+
+    void Run()
+    {
+        _service->run();
+    }
+
+private:
+    hpt::IoServicePtr _service;
+    hpt::SocketPtr _socket;
 };
 
 using EchoClientPtr = std::shared_ptr<EchoClient>;
 
-void RunClient(EchoClientPtr client) {
-  auto addr = hpt::Address::from_string("127.0.0.1");
-  uint16_t port = 8001;
-  hpt::Endpoint endpoint(addr, port);
-  client->TryConnect(endpoint);
-  client->Run();
+void RunClient(EchoClientPtr client)
+{
+    auto addr = hpt::Address::from_string("127.0.0.1");
+    uint16_t port = 8001;
+    hpt::Endpoint endpoint(addr, port);
+    client->TryConnect(endpoint);
+    client->Run();
 }
 
-int main(int argc, char *argv[]) {
-  auto client = std::make_shared<EchoClient>();
-  std::thread t(RunClient, client);
-  t.join();
-  return 0;
+int main(int argc, char* argv[])
+{
+    auto client = std::make_shared<EchoClient>();
+    std::thread t(RunClient, client);
+    t.join();
+    return 0;
 }
--- examples/echo_server.cc
+++ examples/echo_server.cc
@@ -6,67 +6,83 @@
 
 class EchoServer;
 
-class TcpConnection : private hpt::noncopyable, public std::enable_shared_from_this<TcpConnection> {
- public:
-  using SelfType = TcpConnection;
+class TcpConnection
+        : private hpt::noncopyable,
+          public std::enable_shared_from_this<TcpConnection>
+{
+public:
+    using SelfType = TcpConnection;
 
-  TcpConnection(hpt::IoService &service) : _sock(service) {}
+    TcpConnection(hpt::IoService& service) : _sock(service) {}
 
-  hpt::Socket &GetSocket() { return _sock; }
+    hpt::Socket& GetSocket() { return _sock; }
 
-  void OnAccept() {
-    std::cout << "Connection Established! " << std::endl;
-  }
-
- private:
-  hpt::Socket _sock;
-};
-
-class EchoServer : private asio::noncopyable, public std::enable_shared_from_this<EchoServer> {
- public:
-  using SelfType = EchoServer;
-
-  EchoServer() {
-    _service = std::make_shared<hpt::IoService>();
-    _acceptor = std::make_shared<hpt::Acceptor>(*_service);
-  }
-
-  void StartAccept(hpt::Endpoint endpoint) {
-    _acceptor->open(endpoint.protocol());
-    _acceptor->bind(endpoint);
-    _acceptor->listen();
-    AcceptOne();
-  }
-
-  void AcceptOne() {
-    auto conn = std::make_shared<TcpConnection>(*this->_service);
-    _acceptor->async_accept(conn->GetSocket(), MEM_FN(HandleAccept, conn, _1));
-  }
-
-  void HandleAccept(std::shared_ptr<TcpConnection> connection, const asio::error_code &ec) {
-    if (ec) {
-      std::cerr << "Accept Error, " << ec << std::endl;
-      return;
+    void OnAccept()
+    {
+        std::cout << "Connection Established! " << std::endl;
     }
-    connection->OnAccept();
-    AcceptOne();
-  }
 
-  void Run() {
-    hpt::Endpoint endpoint(hpt::Address::from_string("127.0.0.1"), 8001);
-    StartAccept(endpoint);
-    _service->run();
-  }
-
-  hpt::IoService &GetIoService() { return *_service; }
-
- private:
-  hpt::IoServicePtr _service;
-  hpt::AcceptorPtr _acceptor;
+private:
+    hpt::Socket _sock;
 };
 
-int main(int argc, char *argv[]) {
-  auto server = std::make_shared<EchoServer>();
-  server->Run();
-  return 0;
+class EchoServer
+        : private asio::noncopyable,
+          public std::enable_shared_from_this<EchoServer>
+{
+public:
+    using SelfType = EchoServer;
+
+    EchoServer()
+    {
+        _service = std::make_shared<hpt::IoService>();
+        _acceptor = std::make_shared<hpt::Acceptor>(*_service);
+    }
+
+    void StartAccept(hpt::Endpoint endpoint)
+    {
+        _acceptor->open(endpoint.protocol());
+        _acceptor->bind(endpoint);
+        _acceptor->listen();
+        AcceptOne();
+    }
+
+    void AcceptOne()
+    {
+        auto conn = std::make_shared<TcpConnection>(*this->_service);
+        _acceptor->async_accept(conn->GetSocket(),
+                                MEM_FN(HandleAccept, conn, _1));
+    }
+
+    void HandleAccept(std::shared_ptr<TcpConnection> connection,
+                      const asio::error_code& ec)
+    {
+        if (ec)
+        {
+            std::cerr << "Accept Error, " << ec << std::endl;
+            return;
+        }
+        connection->OnAccept();
+        AcceptOne();
+    }
+
+    void Run()
+    {
+        hpt::Endpoint endpoint(hpt::Address::from_string("127.0.0.1"), 8001);
+        StartAccept(endpoint);
+        _service->run();
+    }
+
+    hpt::IoService& GetIoService() { return *_service; }
+
+private:
+    hpt::IoServicePtr _service;
+    hpt::AcceptorPtr _acceptor;
+};
+
+int main(int argc, char* argv[])
+{
+    auto server = std::make_shared<EchoServer>();
+    server->Run();
+    return 0;
 }
