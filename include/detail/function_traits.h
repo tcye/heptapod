754f729e2eedcc3269566b85cc2de2688d321eca
Tiancai Ye <tiancai.ye@outlook.com>
2017-04-19 15:06:02 +0800

    add function_traits for auto extract rpc handler

---
+++ include/detail/function_traits.h
@@ -0,0 +1,60 @@
+//
+// Created by tiancai on 2017/4/19.
+//
+
+#ifndef HEPTAPOD_RPC_HANDLER_TRAITS_H
+#define HEPTAPOD_RPC_HANDLER_TRAITS_H
+
+#include <tuple>
+
+namespace hpt {
+namespace detail {
+
+template<typename T>
+struct IsConstRef
+{
+    static constexpr bool value =
+        std::is_reference<T>::value && std::is_const<std::remove_reference_t<T>>::value;
+};
+
+template<typename T, typename... Args>
+struct IsAllConstRef
+{
+    static constexpr bool value =
+        IsConstRef<T>::value && IsAllConstRef<Args...>::value;
+};
+
+template<typename T>
+struct IsAllConstRef<T>
+{
+    static constexpr bool value = IsConstRef<T>::value;
+};
+
+template<typename T>
+struct FunctionTraits;
+
+template<typename Ret, typename... Args>
+struct FunctionTraits<Ret(Args...)>
+{
+    static constexpr int Arity = sizeof...(Args);
+    using ResultType = Ret;
+
+    template<size_t I>
+    struct Arg
+    {
+        using Type = std::tuple_element<I, std::tuple<Args...>>::type;
+    };
+
+    using DecayedArgs = std::tuple<std::decay_t<Args>...>;
+};
+
+template<typename Functor>
+struct FunctionTraits : FunctionTraits<decltype(&Functor::operator())>
+{
+
+};
+
+} // namespace detail
+} // namespace hpt
+
+#endif //HEPTAPOD_RPC_HANDLER_TRAITS_H
---
+++ include/rpc_context.h
@@ -0,0 +1,14 @@
+//
+// Created by tiancai on 2017/4/19.
+//
+
+#ifndef HEPTAPOD_RPC_CONTEXT_H
+#define HEPTAPOD_RPC_CONTEXT_H
+
+namespace hpt {
+
+class RpcServerStream;
+
+} // namespace hpt
+
+#endif //HEPTAPOD_RPC_CONTEXT_H
